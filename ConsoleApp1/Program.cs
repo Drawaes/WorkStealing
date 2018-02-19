1a3ff60306e44e782b25ba987ea0512816535645
Drawaes <seawardtim@gmail.com>
2018-02-19 13:13:39 +0000

    Testing theory

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Threading;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static ConcurrentQueue<Action>[] _globalQueues = new ConcurrentQueue<Action>[8 >> 2];
+        static Worker[] _threads = new Worker[16];
+        delegate int GetProcDelegate();
+        static GetProcDelegate GetProc;
+
+        static void Main(string[] args)
+        {
+            var environment = Type.GetType("System.Environment");
+            var props = environment.GetMethod("get_CurrentProcessorNumber", BindingFlags.NonPublic | BindingFlags.Static | BindingFlags.FlattenHierarchy);
+            GetProc = (GetProcDelegate) props.CreateDelegate(typeof(GetProcDelegate));
+
+
+            for (var i = 0; i < _globalQueues.Length;i++)
+            {
+                _globalQueues[i] = new ConcurrentQueue<Action>();
+            }
+
+            for(var i = 0; i < _threads.Length;i++)
+            {
+                _threads[i] = new Worker(i, _globalQueues, _threads);
+            }
+            for(var i = 0; i<_threads.Length;i++)
+            {
+                _threads[i].Start();
+            }
+            
+            for(var i = 0; i < _threads.Length;i++)
+            {
+                _threads[i].Thread.Join();
+                Console.WriteLine($"{i + 1} thread finished tasks done {_threads[i].TasksDone}");
+            }
+            Console.WriteLine("Hello World!");
+        }
+
+        // The upper bits of t_executionIdCache are the executionId. The lower bits of
+        // the t_executionIdCache are counting down to get it periodically refreshed.
+        // TODO: Consider flushing the executionIdCache on Wait operations or similar 
+        // actions that are likely to result in changing the executing core
+        [ThreadStatic]
+        static int t_executionIdCache;
+
+        const int ExecutionIdCacheShift = 16;
+        const int ExecutionIdCacheCountDownMask = (1 << ExecutionIdCacheShift) - 1;
+        const int ExecutionIdRefreshRate = 5000;
+
+        private static int RefreshExecutionId()
+        {
+            int executionId = GetProc();
+
+            // On Unix, CurrentProcessorNumber is implemented in terms of sched_getcpu, which
+            // doesn't exist on all platforms.  On those it doesn't exist on, GetCurrentProcessorNumber
+            // returns -1.  As a fallback in that case and to spread the threads across the buckets
+            // by default, we use the current managed thread ID as a proxy.
+            if (executionId < 0) executionId = Environment.CurrentManagedThreadId;
+
+            Debug.Assert(ExecutionIdRefreshRate <= ExecutionIdCacheCountDownMask);
+
+            // Mask with Int32.MaxValue to ensure the execution Id is not negative
+            t_executionIdCache = ((executionId << ExecutionIdCacheShift) & Int32.MaxValue) | ExecutionIdRefreshRate;
+
+            return executionId;
+        }
+
+        // Cached processor number used as a hint for which per-core stack to access. It is periodically
+        // refreshed to trail the actual thread core affinity.
+        public static int CurrentExecutionId
+        {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            get
+            {
+                int executionIdCache = t_executionIdCache--;
+                if ((executionIdCache & ExecutionIdCacheCountDownMask) == 0)
+                    return RefreshExecutionId();
+                return (executionIdCache >> ExecutionIdCacheShift);
+            }
+        }
+    }
+}
---
+++ ConsoleApp1/Worker.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace ConsoleApp1
+{
+    public class Worker
+    {
+        private int _reductionPoint = 5000;
+        private int _tasksDone = 0;
+        private Random _random;
+        private ConcurrentQueue<Action> _localQueue = new ConcurrentQueue<Action>();
+        private ConcurrentQueue<Action>[] _globalQueues;
+        private Worker[] _allWorkers;
+
+        public Worker(int id, ConcurrentQueue<Action>[] globalQueues, Worker[] allWorkers)
+        {
+            _random = new Random(id);
+            _allWorkers = allWorkers;
+            _globalQueues = globalQueues;
+            _localQueue.Enqueue(Sleep);
+
+        }
+
+        public int TasksDone => _tasksDone;
+
+        public Thread Thread { get; set; }
+
+        internal void Start()
+        {
+            var newThread = new Thread(RunThread);
+            newThread.Start();
+            Thread = newThread;
+        }
+
+        private void DecideWhatToDoNext()
+        {
+            _tasksDone++;
+            var rnd = _random.Next(2);
+            if (_tasksDone >= _reductionPoint)
+            {
+                rnd--;
+            }
+            else
+            {
+                rnd++;
+            }
+            if (rnd <= 0) return;
+
+            var whichTask = _random.Next(3);
+            for (var i = 0; i < rnd; i++)
+            {
+                switch (whichTask)
+                {
+                    case 0:
+                        _localQueue.Enqueue(Spin);
+                        break;
+                    case 1:
+                        _localQueue.Enqueue(Sleep);
+                        break;
+                    case 2:
+                        _globalQueues[Program.CurrentExecutionId >> 2].Enqueue(Spin);
+                        break;
+                    case 3:
+                        _globalQueues[Program.CurrentExecutionId >> 2].Enqueue(Sleep);
+                        break;
+                }
+            }
+        }
+
+        public void RunThread(object state)
+        {
+            while (true)
+            {
+                var action = GetNextTask();
+                if (action == null)
+                {
+                    Thread.SpinWait(500);
+                    action = GetNextTask();
+                    if (action == null) return;
+                }
+                action();
+                DecideWhatToDoNext();
+            }
+        }
+
+        public Action GetNextTask()
+        {
+            if (_localQueue.TryDequeue(out var action)) return action;
+
+            //Steal from out "local" global
+            var queueBucket = Program.CurrentExecutionId >> 2;
+            if (_globalQueues[queueBucket].TryDequeue(out action)) return action;
+
+            var rndStart = _random.Next(_allWorkers.Length - 1);
+            var counter = _allWorkers.Length;
+            while(counter > 0)
+            {
+                if (rndStart == _allWorkers.Length) rndStart = 0;
+                if (_allWorkers[rndStart]._localQueue.TryDequeue(out action)) return action;
+                rndStart++;
+                counter--;
+            }
+            return null;
+        }
+
+        public void Sleep() => Thread.Sleep(10);
+        public void Spin() => Thread.SpinWait(1000);
+    }
+}
